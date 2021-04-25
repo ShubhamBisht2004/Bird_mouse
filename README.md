5c7abc971b7b94d6ea06a1b2d140f26a003aa1a8
Shubham Bisht <77498644+ShubhamBisht2004@users.noreply.github.com>
2021-04-25 19:33:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Bisr_mouse
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ SlingShot.js
@@ -0,0 +1,36 @@
+class SlingShot {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.SlingShot = Constraint.create(options);
+        //this.SlingShot = constraint.pointB()
+        World.add(world, this.SlingShot);
+
+
+    }
+
+    fly() {
+
+        this.Slingshot.bodyA = null;
+    }
+
+
+
+    display() {
+        if (this.SlingShot.bodyA) {
+
+
+            var pointA = this.SlingShot.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+
+    }
+
+}
